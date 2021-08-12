c1865c04fce51069d3915fa25f5db30bce7d484f
Jacking Tài <54407688+tai-doan@users.noreply.github.com>
2021-08-12 23:40:58 +0700

    remove file when import success

--- src/components/ImportExcel/index.js
+++ src/components/ImportExcel/index.js
@@ -329,6 +329,7 @@ const ImportExcel = ({ title, onRefresh }) => {
             setEditID(null)
             setEditModal({ ...productDefaulModal })
             setShouldOpenModalEdit(false)
+            setFileSelected('')
             setDataSource([])
             setShouldOpenModal(false)
         }
